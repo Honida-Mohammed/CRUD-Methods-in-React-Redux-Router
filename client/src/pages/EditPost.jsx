77526613d693ddc53b487ee05ae62d9fd3aeb887
Honida Mohammed <honidaomar99@gmail.com>
2023-01-26 20:55:05 +0200

    updated

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 import usePostDetails from "../hooks/use-post-details";
 import { useDispatch } from "react-redux";
-import { editPost, clearRecord} from "../store/postSlice";
+import { editPost } from "../store/postSlice";
 import { useNavigate } from "react-router-dom";
 
 import Loading from "../components/Loading";
 import { Form, Button } from "react-bootstrap";
 
 const EditPost = () => {
-  const { loading: dataLoading, error: dataError, record } = usePostDetails();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { loading, error, record } = usePostDetails();
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  const dispatch = useDispatch(); 
-  const navigate = useNavigate();
+  //record bending null
+  //record false / title true /  description true x
+
+  //record full filled
+  //record true / title true /  description true x
 
   useEffect(() => {
     if (record) {
@@ -23,45 +28,44 @@ const EditPost = () => {
     }
   }, [record]);
 
-  
   useEffect(() => {
     return () => {
-      dispatch(clearRecord())
-    }
-  }, [dispatch])
-
+      dispatch({ type: "posts/cleanRecord" });
+    };
+  }, [dispatch]);
 
   const formHandler = (e) => {
     e.preventDefault();
-    dispatch(editPost({id: record?.id, title, description})).unwrap().then(() => navigate("/"));
+    dispatch(editPost({ id: record.id, title, description }))
+      .unwrap()
+      .then(() => navigate("/"));
   };
 
   return (
-    <Loading loading={dataLoading} error={dataError}>
-      <Form onSubmit={formHandler}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Title</Form.Label>
-          <Form.Control
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </Form.Group>
-
+    <Form onSubmit={formHandler}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </Form.Group>
+      <Loading loading={loading} error={error}>
         <Button variant="primary" type="submit">
           Submit
         </Button>
-      </Form>
-    </Loading>
+      </Loading>
+    </Form>
   );
 };
 
